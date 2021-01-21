c2d2a53dd47c7ecc4603b875c0ffb3770916a540
rpn42rus <68055599+rpn42rus@users.noreply.github.com>
2021-01-21 14:00:14 +0700

    init

---
+++ nuxt.config.js
@@ -0,0 +1,32 @@
+export default {
+  ssr: false,
+
+  head: {
+    title: 'popso',
+    htmlAttrs: {
+      lang: 'ru',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: ['element-ui/lib/theme-chalk/index.css'],
+
+  plugins: ['@/plugins/element-ui'],
+
+  components: true,
+
+  buildModules: ['@nuxtjs/eslint-module'],
+
+  modules: ['@nuxtjs/axios'],
+
+  axios: {},
+
+  build: {
+    transpile: [/^element-ui/],
+  },
+}
